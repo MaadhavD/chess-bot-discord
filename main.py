f7ff7fb2b97ab2f44240a38a9e1774118f1c043f
MaadhavRobotics <maadhavdeekshitha@gmail.com>
2022-12-25 11:46:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import chess
 import chess.pgn
 import chess.svg
 import chess.engine
+import chess.uci
 import tkinter as tk
+import asyncio
 
 # Replace YOUR_BOT_TOKEN with the token for your Discord bot
 TOKEN = 'MTAyNzcyODI3OTY1NTk0NDIzMw.GZzpi7.3Fe6Z0ZTHoRcYiKutF7DYFAYrW9fgrp5s-o4dg'
@@ -13,16 +15,7 @@ TOKEN = 'MTAyNzcyODI3OTY1NTk0NDIzMw.GZzpi7.3Fe6Z0ZTHoRcYiKutF7DYFAYrW9fgrp5s-o4d
 client = discord.Client()
 bot = commands.Bot(command_prefix='!')
 
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.errors.CheckFailure):
-        await ctx.send("Sorry, you don't have permission to use this command.")
-    elif isinstance(error, commands.errors.CommandNotFound):
-        await ctx.send("Sorry, that is not a valid command.")
-    else:
-        await ctx.send("An error has occurred while processing your command.")
-
-
-# Create a chess board and engine
+# Set up the chess board and engine
 board = chess.Board()
 engine = chess.uci.popen_engine("stockfish")
 engine.uci()
@@ -33,10 +26,13 @@ class ChessWidget(tk.Frame):
         super().__init__(master)
         self.pack()
         self.initUI()
-
+    
     def initUI(self):
+        # Generate an SVG image of the chess board
+        self.board_svg = chess.svg.board(board=board, size=400)
+
         # Set up the chess board and pieces
-        self.board = tk.Label(self, image="chessboard.png")
+        self.board = tk.Label(self, image=self.board_svg)
         self.board.pack()
 
         self.pieces = []
@@ -50,72 +46,70 @@ class ChessWidget(tk.Frame):
             piece.bind("<B1-Motion>", self.on_drag_move)
             piece.bind("<ButtonRelease-1>", self.on_drag_release)
 
-    def update_board(self):
-        # Update the positions of the pieces on the board
-        for i, piece in enumerate(self.pieces):
-            row, col = divmod(i, 8)
-            x = col * 50
-            y = row * 50
-            piece.place(x=x, y=y)
 
-            # Get the piece at the current position on the chess board
-            pos = chess.square(i)
-            square = board.piece_at(pos)
-            if square is None:
-                piece.configure(image='')
-            else:
-                piece.configure(image=f"{square.symbol()}.png")
+def update_board(self):
+    # Update the chess board image
+    self.board.configure(image=self.board_svg)
 
-    def on_drag_start(self, event):
+    # Update the positions of the pieces on the board
+    for i, piece in enumerate(self.pieces):
+        row, col = divmod(i, 8)
+        x = col * 50
+        y = row * 50
+        piece.place(x=x, y=y)
+
+        # Get the piece at the current position on the chess board
+        pos = chess.square(i)
+        square = board.piece_at(pos)
+        if square is None:
+            piece.configure(image='')
+        else:
+            piece.configure(image=f"{square.symbol()}.png")
+
+def on_drag_start(self, event):
         # Get the source square for the move
         self.source_square = event.widget
         self.source_square.lift()
         self.drag_x = event.x
         self.drag_y = event.y
-
-    def on_drag_move(self, event):
+    
+def on_drag_move(self, event):
         # Move the piece along with the mouse cursor
         self.source_square.place(x=event.x_root - self.drag_x, y=event.y_root - self.drag_y)
 
-    def on_drag_release(self, event):
+    
+def on_drag_release(self, event):
         # Get the destination square for the move
         self.dest_square = event.widget
+
 # Set up the Discord bot commands
-@bot.command()
-async def newgame(ctx):
+@commands.command(name='newgame', help='Start a new game of chess')
+def newgame(bot, ctx):
     # Start a new game
     board.reset()
     widget.update_board()
 
-@bot.command()
-async def move(ctx, source, dest):
+@commands.command(name='move', help='Make a move on the chess board')
+async def move(bot, ctx, source, dest):
     # Make a move on the chess board
     move = chess.Move.from_uci(f"{source}{dest}")
     if move in board.legal_moves:
-        board.push(move)
-        widget.update_board()
+        # Use asyncio to run the chess engine in the background
+        async def make_move():
+            board.push(move)
+            widget.update_board()
+            engine.position(board)
+            result = engine.go(movetime=2000)
+            board.push(result.bestmove)
+            widget.update_board()
+
+        asyncio.create_task(make_move())
     else:
-        await ctx.send("Invalid move!")
+        await ctx.send("Invalid move")
 
-@bot.command()
-async def showboard(ctx):
-    # Send an image of the current chess board to Discord
-    img = chess.svg.board(board=board, size=400)
-    await ctx.send(file=discord.File(img))
-
-@bot.command()
-async def play(ctx):
-    # Make a move using the chess engine
-    try:
-        result = engine.play(board, chess.engine.Limit(time=0.1))
-        board.push(result.move)
-        widget.update_board()
-        await ctx.send(f"I made the move {result.move}")
-    except chess.engine.EngineError as e:
-        await ctx.send(f"An error occurred while interacting with the chess engine: {e}")
 # Run the Discord bot
-client.run(TOKEN)
-
+bot.run(TOKEN)
+#your mom
 # Set up the tkinter GUI and run the main loop
 root = tk.Tk()
 widget = ChessWidget(master=root)
